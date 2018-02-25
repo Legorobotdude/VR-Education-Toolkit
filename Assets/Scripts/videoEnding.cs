4160eb2687eff90ab2b18e5482df0b2f8eebddf6
Legorobotdude <adityabawankule@gmail.com>
2018-02-24 19:28:46 -0600

    Create videoEnding.cs

---
+++ Assets/Scripts/videoEnding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class videoEnding : MonoBehaviour {
+
+	public float endingTime = 100f;
+	public GameObject sceneObjects;
+	//Component videoPlayer;
+	 private VideoPlayer videoPlayer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Awake()
+	{
+		  videoPlayer = GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		if (videoPlayer.time == endingTime)
+		{
+			sceneObjects.SetActive(true);
+		}
+	}
+}
