38056c7301c17b3c14def69bd67e9bb53c1717f5
Aditya Bawankule <adityabawankule@gmail.com>
2018-02-26 21:06:46 -0600

    Add oscillation script

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+   float movementFactor;//0 for not moved, 1 for fully moved
+
+    [SerializeField] float period;
+
+    Vector3 startingPos;
+    
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+        float cycles = Time.time / period;
+
+        const float tau = 2 * Mathf.PI;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+
+	}
+}
