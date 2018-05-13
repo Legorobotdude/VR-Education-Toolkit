5cdf63fbe4bcf0a64a352bcb19c290db81ead998
Aditya Bawankule <adityabawankule@gmail.com>
2018-05-13 02:40:17 -0500

    Script now randomly generates simple math questions

--- Assets/Scripts/TriviaGame/MathQuestion.cs
+++ Assets/Scripts/TriviaGame/MathQuestion.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MathQuestion : Question {
+public class MathQuestion : Question
+{
+    [SerializeField] private int maxNumber = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        GenerateQuestion();
+    }
 
-	protected override void GenerateQuestion()
-	{
-		QuestionText.text = "This is your question";
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    protected override void GenerateQuestion()
+    {
+        string questionText = "";
+        questionText += Random.Range(0, maxNumber).ToString();
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+                questionText += "+";
+                break;
+            case 1:
+                questionText += "-";
+                break;
+            case 2:
+                questionText += "*";
+                break;
+            case 3:
+                questionText += "/";
+                break;
+        }
+        questionText += Random.Range(0, maxNumber).ToString();
+        questionText += "=?";
+
+        QuestionText.text = questionText;
+    }
 }
