35105f4786c4e815358d72061c064298bfd6ef33
Aditya Bawankule <adityabawankule@gmail.com>
2018-05-13 02:08:55 -0500

    Create mockup scene

--- Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/Gun.cs
+++ Assets/VRTK/LegacyExampleFiles/ExampleResources/Scripts/Gun.cs
@@ -5,8 +5,8 @@
     public class Gun : VRTK_InteractableObject
     {
         private GameObject bullet;
-        private float bulletSpeed = 1000f;
-        private float bulletLife = 5f;
+        [SerializeField]private float bulletSpeed = 1000f;
+        [SerializeField]private float bulletLife = 5f;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
